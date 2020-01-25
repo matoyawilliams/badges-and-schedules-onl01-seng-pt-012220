84a6dc1869ec931b2821faeea7af16d6918f75ff
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-25 23:12:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+badge_maker =Hello, my name is#{name}
+
+batch_badge_creator = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+assign_rooms = ["1","2","3","4","5","6","7"]
+
+assign_rooms.each do |name|
+  
